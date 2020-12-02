53b88096a492f3eb166e765f276057fd488b7f3d
Dylan Eckhart <deckhart@roc-dev.com>
2020-12-02 17:16:30 +0100

    Version 3.1

--- Hoofdstuk 1/definition_start.py
+++ Hoofdstuk 1/definition_start.py
@@ -3,9 +3,14 @@
 
 
 # TODO: create a basic class
-
+class Book:
+    def __init__(self, title):
+        self.title = title
 
 # TODO: create instances of the class
-
+b1 = Book("Brave New World")
+b2 = Book("War And Peace")
 
 # TODO: print the class and property
+print(b1)
+print(b1.title)
