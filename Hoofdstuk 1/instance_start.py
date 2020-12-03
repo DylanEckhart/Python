cffb7d5965c35617ab1a43991b5876737e1eec31
Dylan Eckhart <deckhart@roc-dev.com>
2020-12-03 11:00:13 +0100

    Version 3.3

--- Hoofdstuk 1/instance_start.py
+++ Hoofdstuk 1/instance_start.py
@@ -5,21 +5,33 @@
 class Book:
     # the "init" function is called when the instance is
     # created and ready to be initialized
-    def __init__(self, title):
+    def __init__(self, title, author, pages, price):
         self.title = title
         # TODO: add properties
+        self.author = author
+        self.pages = pages
+        self.price = price
 
     # TODO: create instance methods
+    def getPrice(self):
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
 
+    def setDiscount(self, amount):
+        self._discount = amount
 
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+b1 = Book("War and Peace", "Leo Tolstroy", 1225, 39.95)
+b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
 
 # TODO: print the price of book1
-
+print(b1.getPrice())
 
 # TODO: try setting the discount
-
+print(b2.getPrice())
+b2.setDiscount(0.25)
+print(b2.getPrice())
 
 # TODO: properties with double underscores are hidden by the interpreter
