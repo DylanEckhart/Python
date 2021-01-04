52de3823214365294f54067a23ead65f8ed0c1f4
Dylan Eckhart <deckhart@roc-dev.com>
2021-01-04 11:52:10 +0100

    Version 3.6

--- Hoofdstuk 1/class_start.py
+++ Hoofdstuk 1/class_start.py
@@ -1,13 +1,12 @@
-# Python Object Oriented Programming by Joe Marini course example
-# Using class-level and static methods
-
-
 class Book:
     # TODO: Properties defined at the class level are shared by all instances
-
+        BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
     # TODO: double-underscore properties are hidden from other classes
 
     # TODO: create a class method
+    @classmethod
+    def getbooktypes(cls):
+        return cls.BOOK_TYPES
 
     # TODO: create a static method
 
@@ -16,15 +15,20 @@ class Book:
     def setTitle(self, newtitle):
         self.title = newtitle
 
-    def __init__(self, title):
+    def __init__(self, title, booktype):
         self.title = title
+        if (not booktype in Book.BOOK_TYPES):
+            raise ValueError (f"{booktype} is not a valid boom type")
+        else:
+            self.booktype = booktype
 
 
 # TODO: access the class attribute
+print("Book Types: ", Book.getbooktypes)
 
 
 # TODO: Create some book instances
-b1 = Book("Title 1")
-b2 = Book("Title 2")
+b1 = Book("Title 1", "HARDCOVER")
+b2 = Book("Title 2", "COMIC")
 
 # TODO: Use the static method to access a singleton object
