bf3eae46f32d209621b5b52f54f87748812d8bad
Dylan Eckhart <deckhart@roc-dev.com>
2021-01-19 15:15:22 +0100

    Version 6.1

--- Hoofdstuk 2/multiple_start.py
+++ Hoofdstuk 2/multiple_start.py
@@ -6,17 +6,26 @@ class A:
     def __init__(self):
         super().__init__()
         self.foo = "foo"
+        self.name = "Class A"
 
 
 class B:
     def __init__(self):
         super().__init__()
         self.bar = "bar"
+        self.name = "Class B"
 
 
 class C(A, B):
     def __init__(self):
         super().__init__()
 
+    def showprops(self):
+        print(self.foo)
+        print(self.bar)
+        print(self.name)
+
 
 c = C()
+print(C.__mro__)
+c.showprops()
