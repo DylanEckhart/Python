464a963925534bb707a42e6d5ea1b6c416dc2a36
Dylan Eckhart <deckhart@roc-dev.com>
2021-01-20 08:54:04 +0100

    Version 7.1

--- Hoofdstuk 2/interface_start.py
+++ Hoofdstuk 2/interface_start.py
@@ -4,6 +4,12 @@
 from abc import ABC, abstractmethod
 
 
+class JSONify(ABC):
+    @abstractmethod
+    def toJSON(self):
+        pass
+
+
 class GraphicShape(ABC):
     def __init__(self):
         super().__init__()
@@ -13,13 +19,17 @@ class GraphicShape(ABC):
         pass
 
 
-class Circle(GraphicShape):
+class Circle(GraphicShape, JSONify):
     def __init__(self, radius):
         self.radius = radius
 
     def calcArea(self):
         return 3.14 * (self.radius ** 2)
 
+    def toJSON(self):
+        return f"{{ \"Circle\": {str(self.calcArea())} }}"
+
 
 c = Circle(10)
 print(c.calcArea())
+print(c.toJSON())
