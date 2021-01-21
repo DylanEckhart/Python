274e55df1131a8a5822320a5ab1d46745f53c06c
Dylan Eckhart <deckhart@roc-dev.com>
2021-01-21 09:59:12 +0100

    Version 8.1

--- Hoofdstuk 2/composition_start.py
+++ Hoofdstuk 2/composition_start.py
@@ -3,23 +3,47 @@
 
 
 class Book:
-    def __init__(self, title, price, authorfname, authorlname):
+    def __init__(self, title, price, author=None):
         self.title = title
         self.price = price
 
-        self.authorfname = authorfname
-        self.authorlname = authorlname
-
+        # Use references to other objects, like author and chapters
+        self.author = author
         self.chapters = []
 
-    def addchapter(self, name, pages):
-        self.chapters.append((name, pages))
+    def addchapter(self, chapter):
+        self.chapters.append(chapter)
+
+    def getbookpagecount(self):
+        result = 0
+        for ch in self.chapters:
+            result += ch.pagecount
+        return result
 
 
-b1 = Book("War and Peace", 39.0, "Leo", "Tolstoy")
+class Author:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
 
-b1.addchapter("Chapter 1", 125)
-b1.addchapter("Chapter 2", 97)
-b1.addchapter("Chapter 3", 143)
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
+
+
+class Chapter:
+    def __init__(self, name, pagecount):
+        self.name = name
+        self.pagecount = pagecount
+
+
+auth = Author("Leo", "Tolstoy")
+b1 = Book("War and Peace", 39.95, auth)
+
+b1.addchapter(Chapter("Chapter 1", 104))
+b1.addchapter(Chapter("Chapter 2", 89))
+b1.addchapter(Chapter("Chapter 3", 124))
 
 print(b1.title)
+print(b1.author)
+print(b1.getbookpagecount())
+
