2e2bf9c3dd075bc0b1b1fe5d20931b78caa71f7d
Dylan Eckhart <deckhart@roc-dev.com>
2021-02-01 09:28:27 +0100

    Version 9.1

--- Hoofdstuk 3/magicstr_start.py
+++ Hoofdstuk 3/magicstr_start.py
@@ -1,7 +1,3 @@
-# Python Object Oriented Programming by Joe Marini course example
-# Using the __str__ and __repr__ magic methods
-
-
 class Book:
     def __init__(self, title, author, price):
         super().__init__()
@@ -10,8 +6,12 @@ class Book:
         self.price = price
 
     # TODO: use the __str__ method to return a string
+    def __str__(self):
+        return f"{self.title} by {self.author}, costs {self.price}"
 
     # TODO: use the __repr__ method to return an obj representation
+    def __repr__(self):
+        return f"title={self.title},author={self.author},price={self.price}"
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
