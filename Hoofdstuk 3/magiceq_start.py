f7f0ed45189aa9ba3b93e08f790298baade7a180
Dylan Eckhart <deckhart@roc-dev.com>
2021-02-02 10:00:29 +0100

    Version 10.1

--- Hoofdstuk 3/magiceq_start.py
+++ Hoofdstuk 3/magiceq_start.py
@@ -1,7 +1,3 @@
-# Python Object Oriented Programming by Joe Marini course example
-# Using the __str__ and __repr__ magic methods
-
-
 class Book:
     def __init__(self, title, author, price):
         super().__init__()
@@ -10,10 +6,28 @@ class Book:
         self.price = price
 
     # TODO: the __eq__ method checks for equality between two objects
+    def __eq__(self, value):
+        if not isinstance(value, Book):
+            raise ValueError("Can't compare book to non-book type")
+
+        return (self.title == value. title and
+                self.author == value.author and
+                self.price == value.price)
 
     # TODO: the __ge__ establishes >= relationship with another obj
+    def __ge__(self, value):
+        if not isinstance(value, Book):
+            raise ValueError("Can't compare book to non-book type")
+
+        return self.price >= value.price
+
 
     # TODO: the __lt__ establishes < relationship with another obj
+    def __lt__(self, value):
+        if not isinstance(value, Book):
+            raise ValueError("Can't compare book to non-book type")
+
+        return self.price < value.price
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
@@ -22,9 +36,15 @@ b3 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b4 = Book("To Kill a Mockingbird", "Harper Lee", 24.95)
 
 # TODO: Check for equality
-
+print(b1 == b3)
+print(b1 == b2)
 
 # TODO: Check for greater and lesser value
-
+print(b2 >= b1)
+print(b2 < b1)
+print(b3 >= b2)
 
 # TODO: Now we can sort them too
+books = [b1, b3, b2, b4]
+books.sort()
+print([book.title for book in books])
