5619082ec3aae5faa698903c3f95bb4163c6fec1
Dylan Eckhart <deckhart@roc-dev.com>
2021-02-10 09:51:40 +0100

    Version 13.1

--- Hoofdstuk 4/dataclass_start.py
+++ Hoofdstuk 4/dataclass_start.py
@@ -1,13 +1,14 @@
-# Python Object Oriented Programming by Joe Marini course example
-# Using data classes to represent data objects
+from dataclasses import dataclass
 
-
+@dataclass
 class Book:
-    def __init__(self, title, author, pages, price):
-        self.title = title
-        self.author = author
-        self.pages = pages
-        self.price = price
+    title: str
+    author: str
+    pages: int
+    price: float
+
+    def bookinfo(self):
+        return f"{self.title}, by {self.author}"
 
 
 # create some instances
@@ -19,9 +20,13 @@ print(b1.title)
 print(b2.author)
 
 # TODO: print the book itself - dataclasses implement __repr__
-
+print(b1)
 
 # TODO: comparing two dataclasses - they implement __eq__
-
+b3 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
+print(b1 == b3)
 
 # TODO: change some fields
+b1.title = "Anna Karenina"
+b1.pages = 864
+print(b1.bookinfo())
