b44c4309acdf9e9fa754de24a298a472b757c187
Dylan Eckhart <deckhart@roc-dev.com>
2021-02-10 09:44:09 +0100

    Version 12.1

--- Hoofdstuk 3/magiccall_start.py
+++ Hoofdstuk 3/magiccall_start.py
@@ -1,7 +1,3 @@
-# Python Object Oriented Programming by Joe Marini course example
-# Using the __str__ and __repr__ magic methods
-
-
 class Book:
     def __init__(self, title, author, price):
         super().__init__()
@@ -13,9 +9,16 @@ class Book:
         return f"{self.title} by {self.author}, costs {self.price}"
 
     # TODO: the __call__ method can be used to call the object like a function
+    def __call__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
 # TODO: call the object as if it were a function
+print(b1)
+b1("Anna Karenina", "Leo Tolstoy", 49.95)
+print(b1)
