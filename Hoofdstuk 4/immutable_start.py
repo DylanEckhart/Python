453500115d7bb988b47e84f23f27ae1deed93676
Dylan Eckhart <deckhart@roc-dev.com>
2021-02-11 10:21:24 +0100

    Version 15.1

--- Hoofdstuk 4/immutable_start.py
+++ Hoofdstuk 4/immutable_start.py
@@ -1,6 +1,3 @@
-# Python Object Oriented Programming by Joe Marini course example
-# Creating immutable data classes
-
 from dataclasses import dataclass
 
 
@@ -9,11 +6,16 @@ class ImmutableClass:
     value1: str = "Value 1"
     value2: int = 0
 
+    def somefunc(self, newval):
+        self.value2 = newval
 
 obj = ImmutableClass()
 print(obj.value1)
 
 # TODO: attempting to change the value of an immutable class throws an exception
+obj.value1 = "Another Value"
+print(obj.value1)
 
 
 # TODO: even functions within the class can't change anything
+obj.somefunc(20)
